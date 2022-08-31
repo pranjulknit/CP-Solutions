6207e4365cc5ff417b3c9e42bda25f3132cfb0de
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:47:29 +0530

    Committing files

---
+++ CodeForces/GNU C++17/242B | Big Segment/167719297.cpp
@@ -0,0 +1,79 @@
+// Problem: B. Big Segment
+// Contest: Codeforces - Codeforces Round #149 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/242/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(a==0 or b==0)return 0;if(b%a==0){return a;}gcd(b%a,b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define MX  30009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ 
+ 
+bool comp(pll &a,pll &b){
+ if(b.second>= a.second and a.first>= b.first){
+  return true;
+ }
+ 
+ return false;
+}
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ ll n;
+ cin>>n;
+ 
+ 
+ vector<pll> v(n),temp;
+ 
+ for(ll i=0;i<n;i++){
+  cin>>v[i].first>>v[i].second;
+ }
+ temp = v;
+ sort(v.begin(),v.end(),comp);
+ ll i;
+ ll ans = -1;
+ for(i=0;i<n;i++){
+  if(v[n-1].first == temp[i].first and v[n-1].second == temp[i].second){
+   ans = i;
+  }
+  else if(v[n-1].first<=temp[i].first and v[n-1].second>=temp[i].second){
+   continue;
+  }
+  else{
+   break;
+  }
+ }
+ if(i==n){
+  cout<<ans+1<<endl;
+  return 0;
+ }
+ cout<<-1<<endl;
+ return 0;
+}
