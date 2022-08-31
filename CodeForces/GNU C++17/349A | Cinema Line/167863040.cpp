f27f69140e558e8e3baeb650c057fbdda1246778
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:46:16 +0530

    Committing files

---
+++ CodeForces/GNU C++17/349A | Cinema Line/167863040.cpp
@@ -0,0 +1,126 @@
+// Problem: A. Cinema Line
+// Contest: Codeforces - Codeforces Round #202 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/349/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(a==0 or b==0)return 0;if(b%a==0){return a;}gcd(b%a,b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define MX  3000009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ ll n;
+ cin>>n;
+ 
+ 
+ 
+ vector<ll> v(n);
+ 
+ vector<ll> prefix(n+1,0);
+ 
+ for(ll i=0;i<n;i++){
+  cin>>v[i];
+  
+ }
+ 
+ 
+ if(v[0]>25){
+  cout<<"NO"<<endl;
+  return 0;
+ }
+ ll sum = v[0];
+ ll de[101]={0};
+ de[25]++;
+ ll sau = 0;
+ for(ll i=1;i<n;i++){
+  
+  if(v[i] == 25){
+   de[25]++;
+   
+  }
+  
+  else if(v[i]==50){
+   de[50]++;
+   
+   if(de[25]<=0){
+    cout<<"NO"<<endl;
+    return 0;
+   }
+   de[25]--;
+  }
+  else if(v[i]== 75){
+   de[75]++;
+   
+   
+   if(de[50]>0){
+    de[50]--;
+    
+   }
+   else if(de[25]>=2){
+    de[25]-=2;
+   }
+   else{
+    cout<<"NO"<<endl;
+    return 0;
+   }
+  }
+  else if(v[i]==100){
+   de[100]++;
+   
+   if(de[75]>0){
+    de[75]--;
+   }
+   else if(de[50]>0 and de[25]>0){
+    de[50]-=1;
+    de[25]-=1;
+   }
+   else if(de[25]>=3){
+    de[25]-=3;
+   }
+   else{
+    cout<<"NO"<<endl;
+    return 0;
+   }
+  }
+  
+  
+ }
+ 
+ cout<<"YES"<<endl;
+ return 0;
+}
