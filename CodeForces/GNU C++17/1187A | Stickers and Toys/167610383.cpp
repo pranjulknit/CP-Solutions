aaae8c2d7d9e05ca7e2463554d465f7ff0625139
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:48:16 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1187A | Stickers and Toys/167610383.cpp
@@ -0,0 +1,63 @@
+// Problem: A. Stickers and Toys
+// Contest: Codeforces - Educational Codeforces Round 67 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/1187/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(a==0 or b==0)return 0;if(b%a==0){return a;}gcd(b%a,b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define MX  3000009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ll mod = 100000000003;
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n,s,t;
+  cin>>n>>s>>t;
+  
+  
+  
+  // sticekr skip kr skta hai
+  ll sticks = n-s;
+  ll ticks= n-t;
+  
+  cout<<max(sticks,ticks)+1<<endl;
+ }
+ 
+ 
+ 
+ 
+ return 0;
+}
