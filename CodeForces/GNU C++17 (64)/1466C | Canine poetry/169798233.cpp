34324f086902069f62d81217c40b2815cede2427
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:38:06 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1466C | Canine poetry/169798233.cpp
@@ -0,0 +1,93 @@
+// Problem: C. Canine poetry
+// Contest: Codeforces - Good Bye 2020
+// URL: https://codeforces.com/problemset/problem/1466/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+ 
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(a==0 or b==0)return 0;if(b%a==0){return a;}gcd(b%a,b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define ff first
+#define ss second
+#define MX  3000009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+inline ll form(ll n){return n*(n+1)/2;}
+ 
+ 
+ 
+ 
+int main(){
+ 
+ 
+ ll t;
+ cin>>t;
+ 
+ 
+ while(t--){
+  string s;
+  cin>>s;
+  
+  ll n = s.size();
+  
+  if(n==1){
+   cout<<0<<endl;
+   continue;
+  }
+  
+  if(n==2){
+   if(s[0]==s[1]){
+    cout<<1<<endl;
+    
+   }
+   else{
+    cout<<0<<endl;
+   }
+   continue;
+  }
+  ll cnt=0;
+  for(ll i=1;i<n;i++){
+   
+    if(s[i]== s[i-1]){
+    s[i]='$';
+    
+    
+   }
+   else if(i-2>=0 and s[i]==s[i-2]){
+   
+    s[i]='$';
+    
+   }
+   
+  }
+  
+  for(ll i=0;i<n;i++){
+   if(s[i]=='$'){
+    cnt++;
+   }
+  }
+  
+  cout<<cnt<<endl;
+ }
+ return 0;
+}
