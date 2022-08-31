b3ff6d9f523e30e9a4448783979189290e43decf
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:16:15 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1553A | Digits Sum/127322133.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+// counting divisors
+#define ll long long int
+ 
+using namespace std;
+ 
+ 
+int main(){
+  ll t;
+   cin>>t;
+ 
+   while(t--){
+    ll n;
+    cin>>n;
+ 
+   
+      cout<<(n+1)/10<<"\n";
+   }
+return 0;
+}
