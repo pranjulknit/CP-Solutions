0c456be65f5fa59c875c353183df0d9c244a3ed8
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:57:25 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1324C | Frog Jumps/165763770.cpp
@@ -0,0 +1,58 @@
+// Problem: C. Frog Jumps
+// Contest: Codeforces - Codeforces Round #627 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/1324/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+ 
+ 
+using namespace std;
+ 
+ 
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+inline ll form(ll n){return n*(n+1)/2;}
+ 
+ 
+ 
+ 
+ int main(){
+  ll t;
+  cin>>t;
+  
+  while(t--){
+   string s;
+   cin>>s;
+   ll previ=0;
+   ll mx = 0;
+   for(ll i=1;i<=s.size();i++){
+    if(s[i-1]=='R'){
+     mx = max(mx,i-previ);
+     previ=i;
+    }
+   }
+   mx = max(mx,s.size()+1-previ);
+   if(mx==0){
+    cout<<s.size()+1<<endl;
+    continue;
+   }
+   cout<<mx<<endl;
+   
+  }
+  
+  
+  return 0;
+ }
