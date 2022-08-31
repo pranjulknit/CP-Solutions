8830a4514b3553c9355d19564ddc8210ec5d3adc
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:11:39 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1651A | Playoff/162206966.cpp
@@ -0,0 +1,30 @@
+// Problem: A. Playoff
+// Contest: Codeforces - Educational Codeforces Round 124 (Rated for Div. 2)
+// URL: https://codeforces.com/problemset/problem/1651/A
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define INF 1e9
+using namespace std;
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+  
+  cout<<(1<<n)-1<<"\n";
+ }
+ return 0;
+}
