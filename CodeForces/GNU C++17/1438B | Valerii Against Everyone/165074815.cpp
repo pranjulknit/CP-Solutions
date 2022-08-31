8a5fbb03d8e7f4c07660a7d060cff49214bffc64
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:00:21 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1438B | Valerii Against Everyone/165074815.cpp
@@ -0,0 +1,66 @@
+// Problem: B. Valerii Against Everyone
+// Contest: Codeforces - Codeforces Round #682 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1438/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+ 
+ 
+using namespace std;
+ 
+int solve(string &s,ll n){
+ 
+ if(n==0 || n==1){
+  return 0;
+ }
+    
+ 
+   // cout<<n<<endl;
+ if(s[n-1]=='1' && s[n-2]=='1'){
+         return 1+solve(s,n-1);
+ }
+ else{
+  return solve(s,n-1);
+ }
+ 
+}
+ 
+int main(){
+ 
+  ll t;
+  cin>>t;
+  
+  while(t--){
+   ll n;
+   cin>>n;
+   
+   vector<ll> v(n);
+   //vector<ll> prefix(n+1,0);
+     map<ll,ll> mp;
+     
+     ll cnt= 0;
+   for(ll i=0;i<n;i++){
+    cin>>v[i];
+    mp[v[i]]++;
+    if(mp[v[i]]>1){
+     cnt=2;
+    }
+   }
+   
+   if(cnt){
+    cout<<"YES"<<endl;
+   }
+   else{
+    cout<<"NO"<<endl;
+   } 
+ 
+  }
+ }
