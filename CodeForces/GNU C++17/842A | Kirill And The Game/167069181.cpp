4b0afb3b2bc1f97e525aef6a95c589cb1fb0f5e2
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:50:40 +0530

    Committing files

---
+++ CodeForces/GNU C++17/842A | Kirill And The Game/167069181.cpp
@@ -0,0 +1,70 @@
+// Problem: A. Kirill And The Game
+// Contest: Codeforces - Codeforces Round #430 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/842/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define alli(x,k) x.begin()+k,x.end()
+ 
+ 
+using namespace std;
+ 
+ 
+ 
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+inline ll form(ll n){return n*(n+1)/2;}
+#define line cout<<"line"<<endl
+//power of large number
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ 
+ ll l,r,x,y,k;
+ 
+ cin>>l>>r>>x>>y>>k;
+ 
+ 
+ if(r*k<x){
+  cout<<"NO"<<endl;
+ }
+ else{
+  // rk range me hai ya aage hai
+ for(ll i=x;i<=y;i++){
+     if(i*k>=l and i*k<=r){
+      cout<<"YES"<<endl;
+      return 0;
+     }
+    }
+    cout<<"NO"<<endl;
+    return 0;
+   }
+  
+  
+ 
+ 
+ return 0;
+}
