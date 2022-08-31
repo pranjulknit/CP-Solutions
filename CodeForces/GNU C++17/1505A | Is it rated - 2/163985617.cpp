f3e5eea19346dcf256888da5c9dff166a7ababe7
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:04:17 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1505A | Is it rated - 2/163985617.cpp
@@ -0,0 +1,29 @@
+// Problem: A. Is it rated - 2
+// Contest: Codeforces - April Fools Day Contest 2021
+// URL: https://codeforces.com/problemset/problem/1505/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+ 
+ 
+using namespace std;
+ 
+ 
+ 
+int main(){
+ 
+ string s;
+ 
+ while(cin>>s){
+  cout<<"NO"<<endl;
+  cout.flush();
+ }
+ return 0;}
