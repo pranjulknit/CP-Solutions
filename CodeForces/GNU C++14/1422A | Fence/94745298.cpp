5dbd3ef704625d2c3d0332a2db23984d736b2293
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:18:06 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1422A | Fence/94745298.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll  long long int
+#define out  cout<<
+ 
+using namespace std;
+ 
+ 
+ 
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+       ll a,b,c;
+       cin>>a>>b>>c;
+       cout<<a+b+c-2<<endl;
+       }
+ 
+ 
+return 0;}
