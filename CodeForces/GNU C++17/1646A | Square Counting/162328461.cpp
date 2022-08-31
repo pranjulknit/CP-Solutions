34298a368c76203a2316d5ce6c433c708d93ddcd
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:10:25 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1646A | Square Counting/162328461.cpp
@@ -0,0 +1,24 @@
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define INF 1e9
+using namespace std;
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll a,b;
+  cin>>a>>b;
+  
+  cout<<(b/(a*a))<<"\n";
+ }
+ return 0;
+}
