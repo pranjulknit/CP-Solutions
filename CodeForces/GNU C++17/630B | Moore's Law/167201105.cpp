04e4ca567b0fe0b11a60a486a2d6ea03cb22a3cd
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:49:52 +0530

    Committing files

---
+++ CodeForces/GNU C++17/630B | Moore's Law/167201105.cpp
@@ -0,0 +1,51 @@
+// Problem: B. Moore's Law
+// Contest: Codeforces - Experimental Educational Round: VolBIT Formulas Blitz
+// URL: https://codeforces.com/problemset/problem/630/B
+// Memory Limit: 64 MB
+// Time Limit: 500 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+ 
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ double n,x;
+ cin>>n>>x;
+ 
+ 
+ 
+cout<<fixed<<setprecision(12)<<(double)n*pow(1.000000011,x)<<endl;
+ 
+ 
+ return 0;
+}
