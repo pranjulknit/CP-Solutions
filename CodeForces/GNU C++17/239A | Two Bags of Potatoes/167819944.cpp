de1740cd93c296d37eb35eb2c4751fa4822e7e32
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:46:57 +0530

    Committing files

---
+++ CodeForces/GNU C++17/239A | Two Bags of Potatoes/167819944.cpp
@@ -0,0 +1,82 @@
+// Problem: A. Two Bags of Potatoes
+// Contest: Codeforces - Codeforces Round #148 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/239/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(a==0 or b==0)return 0;if(b%a==0){return a;}gcd(b%a,b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define MX  3000009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ fast();
+ 
+ ll y,k,n;
+ cin>>y>>k>>n;
+ if(y>=n){
+  cout<<-1<<endl;
+  return 0;
+ }
+ 
+ 
+ ll start = ((y+k-1)/k)*k;
+ ll end = (n/k)*k;
+ 
+ 
+ vector<ll> ans;
+ 
+ for(ll i=start;i<=end;i+=k){
+  
+  if((i-y)>0){
+   ans.push_back((i-y));
+  }
+ }
+ 
+ if(ans.size()==0){
+  cout<<-1<<endl;
+  return 0;
+ }
+ 
+ 
+ for(auto &x:ans){
+  cout<<x<<" ";
+ }
+ cout<<endl;
+ 
+ return 0;
+}
