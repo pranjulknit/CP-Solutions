1abd9a58405bb14f50a6c211964dbc11b9bce502
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:37:03 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1612C | Chat Ban/170040287.cpp
@@ -0,0 +1,111 @@
+// Problem: C. Chat Ban
+// Contest: Codeforces - Educational Codeforces Round 117 (Rated for Div. 2)
+// URL: https://codeforces.com/problemset/problem/1612/C
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+ 
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(a==0 or b==0)return 0;if(b%a==0){return a;}gcd(b%a,b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define ff first
+#define ss second
+#define MX  3000009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+inline ll form(ll n){return n*(n+1)/2;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ ll t;
+ cin>>t;
+ 
+ 
+ while(t--){
+  ll k,x;
+  cin>>k>>x;
+  
+  if(k*k<=x){
+   cout<<2*k-1<<endl;
+   continue;
+  }
+  
+  
+  if(form(k)==x){
+   cout<<k<<endl;
+   continue;
+  }
+  
+  if(form(k)>x){
+   
+   ll low = 1;
+   ll high = k;
+   ll ans;
+   
+   while(low<=high){
+    ll mid = low+ (high-low)/2;
+    //cout<<mid<<endl;
+    if(form(mid)>=x){
+     //cout<<mid<<endl;
+     ans = mid;
+     high = mid-1;
+    }
+    else{
+     low = mid+1;
+    }
+   }
+   
+   cout<<ans<<endl;
+   continue;
+  }
+   
+  ll low = 1;
+  ll high = k-1;
+  ll ans;
+  ll check =  x-form(k);
+  ll num = form(k-1);
+  while(low<=high){
+   ll mid = low + (high-low)/2;
+   
+   if(num-form(k-1-mid)>=check){
+    //cout<<mid<<endl;
+    ans = mid;
+    high = mid-1;
+   }
+   else{
+    low = mid+1;
+   }
+  }
+  
+  cout<<k+ans<<endl;
+ }
+ 
+ 
+ return 0;
+}
