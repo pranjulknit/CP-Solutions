61f8f3356e583b26a7b1052f5abde28f5581dc86
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:50:30 +0530

    Committing files

---
+++ CodeForces/GNU C++17/600B | Queries about less or equal elements/167087747.cpp
@@ -0,0 +1,72 @@
+// Problem: B. Queries about less or equal elements
+// Contest: Codeforces - Educational Codeforces Round 2
+// URL: https://codeforces.com/problemset/problem/600/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+ 
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ fast();
+ ll n,m;
+ cin>>n>>m;
+ 
+ vector<ll> a(n),b(m);
+ 
+ 
+ for(ll i=0;i<n;i++){
+  cin>>a[i];
+ }
+ 
+  
+ for(ll i=0;i<m;i++){
+  cin>>b[i];
+ }
+ 
+ 
+ sort(all(a));
+ 
+ ll cnt=0;
+ for(ll i=0;i<m;i++){
+  ll index= upper_bound(all(a),b[i])-a.begin();
+  
+  cout<<index<<" ";
+ }
+ 
+ cout<<endl;
+ 
+ }
