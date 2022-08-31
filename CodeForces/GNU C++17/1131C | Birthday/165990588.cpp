25249d5a0564c6e5212c9c67ecd3663b1be80e2a
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:55:28 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1131C | Birthday/165990588.cpp
@@ -0,0 +1,60 @@
+// Problem: C. Birthday
+// Contest: Codeforces - Codeforces Round #541 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1131/C
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+ 
+ 
+using namespace std;
+ 
+ 
+ 
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+inline ll form(ll n){return n*(n+1)/2;}
+//power of large number
+ 
+ 
+ 
+int main(){
+ ll n;
+ cin>>n;
+ 
+ vector<ll> v(n);
+ 
+ 
+ for(ll i=0;i<n;i++){
+  cin>>v[i];
+ }
+ 
+ sort(all(v));
+ 
+ 
+ for(ll i=1;i<=n;i++){
+  if(i&1){
+   cout<<v[i-1]<<" ";
+  }
+ }
+ 
+ for(ll i=n;i>=1;i--){
+  if(i%2==0){
+   cout<<v[i-1]<<" ";
+  }
+ }
+ 
+ return 0;
+}
