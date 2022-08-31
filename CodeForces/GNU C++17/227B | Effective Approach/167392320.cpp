02dbc0f16df1fb7ce9fc53ea22ea7b07c17c6bfb
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:49:25 +0530

    Committing files

---
+++ CodeForces/GNU C++17/227B | Effective Approach/167392320.cpp
@@ -0,0 +1,80 @@
+// Problem: B. Effective Approach
+// Contest: Codeforces - Codeforces Round #140 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/227/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fast()  ios_base::sync_with_stdio(false);cin.tie(0)
+ 
+using namespace std;
+ 
+ 
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+#define line cout<<"line"<<endl
+#define MX  10000009
+inline bool isSubstring(string s,string check){if(s.find(check)!= string::npos){return true;}return false;}
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ 
+ ll n;
+ cin>>n;
+ vector<ll> v(n);
+ 
+ map<ll,ll> mp;
+ 
+ 
+ for(ll i=0;i<n;i++){
+  cin>> v[i];
+  mp[v[i]]=i;
+ }
+ 
+ 
+ 
+ ll m;
+ cin>>m;
+ ll vasya = 0,petya = 0;
+ while(m--){
+  ll x;
+  cin>>x;
+  
+  vasya += mp[x]+1;
+  
+  petya  += n-mp[x];
+ 
+ 
+ 
+  
+  
+ }
+ 
+ 
+ 
+ 
+ cout<<vasya<<" "<<petya<<endl;
+ 
+ 
+ return 0;
+}
