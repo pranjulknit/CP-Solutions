683d1ee7b6bfe9815b52500828025c661d3aa7a7
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:11:56 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1665A | GCD vs LCM/162031606.cpp
@@ -0,0 +1,29 @@
+// Problem: A. GCD vs LCM
+// Contest: Codeforces - Codeforces Round #781 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1665/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define INF 1e9
+using namespace std;
+ 
+ 
+ 
+ 
+ 
+int main(){
+  ll t;
+  cin>>t;
+  
+  while(t--){
+   ll n;
+   cin>>n;
+   
+   cout<<1<<" "<<n-3<<" "<<1<<" "<<1<<"\n";
+  }
+ return 0;
+}
