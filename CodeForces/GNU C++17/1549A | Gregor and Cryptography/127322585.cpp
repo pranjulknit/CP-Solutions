dce4e0868901424319fa08e0aec85dadd61f0eed
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:16:12 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1549A | Gregor and Cryptography/127322585.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+// counting divisors
+#define ll long long int
+ 
+using namespace std;
+ 
+ 
+int main(){
+  ll t;
+   cin>>t;
+ 
+   while(t--){
+    ll n;
+    cin>>n;
+ 
+    cout<<2<<" "<<n-1<<"\n";
+ 
+    // if(n%2 == n%(n-1))
+    //  cout<<"true"<<"\n";
+   }
+return 0;
+}
