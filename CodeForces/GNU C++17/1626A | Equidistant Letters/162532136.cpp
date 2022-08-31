be90d1ffc52984e522ab2ce3de244b3c92bdd90f
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 19:09:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1626A | Equidistant Letters/162532136.cpp
@@ -0,0 +1,34 @@
+// Problem: A. Equidistant Letters
+// Contest: Codeforces - Educational Codeforces Round 121 (Rated for Div. 2)
+// URL: https://codeforces.com/problemset/problem/1626/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define INF 1e9
+using namespace std;
+ 
+ 
+ 
+ 
+ 
+int main(){
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  string s;
+  cin>>s;
+  
+  map<char,ll> mp;
+  
+  
+  sort(s.begin(),s.end());
+  
+  cout<<s<<"\n";
+ }
+ return 0;
+}
