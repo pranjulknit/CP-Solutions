62db586fd8399cc80a3b76143e30efe6a8427176
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2022-08-31 18:51:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1305B | Kuroni and Simple Strings/167063183.cpp
@@ -0,0 +1,84 @@
+// Problem: B. Kuroni and Simple Strings
+// Contest: Codeforces - Ozon Tech Challenge 2020 (Div.1 + Div.2, Rated, T-shirts + prizes!)
+// URL: https://codeforces.com/problemset/problem/1305/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pll pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define alli(x,k) x.begin()+k,x.end()
+ 
+ 
+using namespace std;
+ 
+ 
+ 
+inline bool isSquare(ll x){ll y = sqrt(x); return y*y == x;}
+inline ll gcd(ll a,ll b){if(b==0){return a;}gcd(b,a%b);}
+inline ll uppr_bound(vector<ll> &v ,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]<=key){low = mid+1;}else{high = mid;}}return low;}
+inline ll lowr_bound(vector<ll> &v,ll key){ll low = 0,high = v.size();while(low<high){ll mid = low + (high-low)/2;if(v[mid]>=key){high = mid;}else{low = mid+1;}}return low;}
+inline ll form(ll n){return n*(n+1)/2;}
+#define line cout<<"line"<<endl
+//power of large number
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+ 
+ string s;
+ cin>>s;
+ 
+ ll n= s.size();
+ vector<ll> index;
+ bool done = false;
+ 
+ ll cnt = 0;
+ 
+ 
+ ll left = 0;
+ ll right = n-1;
+ 
+ while(left<right){
+  if(s[left] == '(' and s[right]==')'){
+   cnt+=2;
+   index.push_back(left+1);
+   index.push_back(right+1);
+   left++,right--;
+  }
+  else if(s[left]=='('){
+   right--;
+  }
+  else{
+   left++;
+  }
+ }
+ 
+ if(cnt==0){
+  cout<<0<<endl;
+  return 0;
+ }
+ cout<<1<<endl;
+ 
+ cout<<cnt<<endl;
+ sort(all(index));
+ for(auto &x:index){
+  cout<<x<<" ";
+ }
+ cout<<endl;
+ 
+ return 0;
+}
